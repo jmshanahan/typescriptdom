cd5e09efa53e69afe467840099e614a09460f56e
jmshanahan <comeraghsolutions@gmail.com>
2023-03-11 10:52:08 +1100

    Filered projects

--- src/app.ts
+++ src/app.ts
@@ -129,7 +129,14 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
     projectState.addListners((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        } else {
+          return prj.status === ProjectStatus.finished;
+        }
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
     this.attach();
@@ -137,8 +144,9 @@ class ProjectList {
   }
   private renderProjects() {
     const listEl = document.getElementById(
-      `${this.type}-projects`
+      `${this.type}-projects-list`
     )! as HTMLUListElement;
+    listEl.innerHTML = "";
     for (const priItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = priItem.title;
